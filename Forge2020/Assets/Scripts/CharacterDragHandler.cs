b2b670f7124c040225e38de7924f92a88e89a901
magictrashbag <magictrashbag@gmail.com>
2019-11-11 22:50:00 +0100

    initial

---
+++ Forge2020/Assets/Scripts/CharacterDragHandler.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CharacterDragHandler : MonoBehaviour 
+{
+    bool isHeld = false;
+    bool isDragging = false;
+    Vector3 originalPosition;
+    Button test;
+
+    private void Start()
+    {
+        originalPosition = transform.position;
+    }
+
+    public void OnDown()
+    {
+        Debug.Log("OnDown");
+        isHeld = true;
+    }
+
+    public void OnDrag()
+    {
+        Debug.Log("OnDrag");
+        isDragging = true;
+        transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    public void OnUp()
+    {
+        Debug.Log("OnUp");
+        transform.position = originalPosition;
+        isHeld = false;
+        bool isDragging = false;
+    }
+}
